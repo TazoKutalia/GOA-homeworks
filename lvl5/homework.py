9c282d17741b57e996fb46e9de5f2c674368869d
TazoKutalia <kutaliatazo8@gmail.com>
2024-05-28 17:15:00 +0400

    sixth commit

---
+++ lvl5/homework.py
@@ -0,0 +1,18 @@
+name="Tazo"
+print(name)
+surname="Kutalia"
+print(surname)
+age=15
+print(age)
+hobby="playing rugby"
+print(hobby)
+temperature=23
+print(temperature)
+temperature=temperature * 3
+print(temperature)
+temperature=temperature / 3
+print(temperature)
+temperature=temperature + 11
+print(temperature)
+temperature=temperature - 2
+print(temperature)
