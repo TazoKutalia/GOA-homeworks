0e0b0590fcfd299917aacafee618079da97aae3c
TazoKutalia <kutaliatazo8@gmail.com>
2024-10-06 19:47:43 +0400

    twenty fourth commit

---
+++ lvl40/cw.py
@@ -0,0 +1,8 @@
+def no_odds(values):
+    even_values = []
+    for num in arr:
+        if num % 2 == 0:
+            even_values.append(num)
+    return even_values
+
+numbers = [1, 2, 3, 4, 5, 6]
---
+++ lvl43/cw/cw.js
@@ -0,0 +1,29 @@
+const friend1 = {
+    firstname: "zura",
+    lastname: "morchiladze",
+    age: 15,
+    hobby: "playing rugby"
+
+
+}
+const friend2 = {
+    firstname: "dato",
+    lastname: "wanturia",
+    age:15,
+    hobby: "reading books"
+
+
+
+
+}
+const friend3 = {
+    firstname: "saba",
+    lastname: "qoiava",
+    age: 15,
+    hobby:"programming"
+
+
+
+}
+console.log(friend1.firstname, friend2.firstname, friend3.firstname)
+console.log(friend1, friend2, friend3)
